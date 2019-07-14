a59e499e49e773cca2a0c331c7c5ccd3f8453d52
MauricioAcosta <andres.taamap@gmail.com>
2019-07-13 22:31:12 -0500

    se modifica el dataset inicial y se crea un nuevo dataset, implementación de la red neuronal con la camara del computador, utlizando la libreria de python3.7 CV2

--- conectCam.py
+++ conectCam.py
@@ -1,13 +1,17 @@
-
 import cv2
+import time
+from Predict import predict
 
 def show_webcam(mirror=False):
     cam = cv2.VideoCapture(0)
     while True:
+        time.sleep(1)
         ret_val, img = cam.read()
         if mirror: 
             img = cv2.flip(img, 1)
         cv2.imshow('my webcam', img)
+        print('img.shape', img.shape)
+        predict(img)
         if cv2.waitKey(1) == 27: 
             break  # esc to quit
     cv2.destroyAllWindows()
---
+++ const.py
@@ -0,0 +1,5 @@
+TEST_DIR = './testDir'
+TRAIN_DIR = './trainDir'
+LEARNING_RATE = 1e-3
+MODEL_NAME = "hapiness-{}-{}.model".format(LEARNING_RATE, "6convfire")
+IMAGE_SIZE = 100
